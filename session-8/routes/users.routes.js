ec330e1a1d17e6b113306d756fc3740bbbfcb351
Sondos.Khalid.444@gmail.com <Sondos.Khalid.444@gmail.com>
2025-08-24 18:46:51 +0300

    edited readmefile

--- session-8/routes/users.routes.js
+++ session-8/routes/users.routes.js
@@ -3,6 +3,6 @@ const router= express.Router()
 const userControllers=require('../controllers/users.controller')
 
 router.post("/signup", userControllers.signup)
-router.put("/addfriends/:userId", userControllers.addFriends)
+router.put("/:userId", userControllers.addFriends)
 
 module.exports=router
